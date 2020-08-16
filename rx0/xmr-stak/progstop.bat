df021ac774dd6ed54b2f1ffc8f308137feab747c
krsysorg <64913036+krsysorg@users.noreply.github.com>
2020-08-16 21:09:09 +0800

    Create progstop.bat

---
+++ rx0/xmr-stak/progstop.bat
@@ -0,0 +1,10 @@
+call ..\..\env\config.bat 
+
+
+@taskkill /T /F /IM xmr-stak-rx.exe 
+ping 127.0.0.1 -n 1
+@taskkill /T /F /IM xmr-stak-rx.exe 
+ping 127.0.0.1 -n 1
+@taskkill /T /F /IM xmr-stak-rx.exe 
+ping 127.0.0.1 -n 1
+@taskkill /T /F /IM xmr-stak-rx.exe 
